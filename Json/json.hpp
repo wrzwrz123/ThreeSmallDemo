0326e3c5898ebfcc5f5e1c0d300ea753a4f7735e
wrzwrz123 <2482438104@qq.com>
2023-07-12 16:50:41 +0800

    add-json.hpp

--- Json/json.hpp
+++ Json/json.hpp
@@ -1,5 +1,281 @@
 #ifndef _JSON_HPP_
 #define _JSON_HPP_
+#include <iostream>
+#include <variant>
+#include <vector>
+#include <map>
+#include <optional>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+namespace json {
+    struct Node;
+    using Null    = std::monostate;
+    using Bool    = bool;
+    using Int     = int64_t;
+    using Float   = double;
+    using String  = std::string;
+    using Array   = std::vector<Node>;
+    using Object  = std::map<std::string, Node>;
+    using Value   = std::variant<Null, Bool, Int, Float, String, Array, Object>;
+
+    struct Node {
+
+        Value value;
+
+
+        Node(Value _value) : value(_value) {}
+        Node() : value(Null{}) {}
+
+
+        auto& operator[](const std::string& key) {
+            if (auto object = std::get_if<Object>(&value)) {
+                return  (*object)[key];
+            }
+            throw std::runtime_error("not an object");
+        }
+
+        auto operator[](size_t index) {
+            if (auto array = std::get_if<Array>(&value)) {
+                return array->at(index);
+            }
+            throw std::runtime_error("not an array");
+        }
+
+        void push(const Node& rhs) {
+            if (auto array = std::get_if<Array>(&value)) {
+                array->push_back(rhs);
+            }
+        }
+    };
+
+    struct json_parser {
+        std::string_view json_str;
+        size_t pos = 0;
+
+
+        void parse_whitespace() {
+            while (pos < json_str.size() && std::isspace(json_str[pos])) {
+                pos++;
+            }
+        }
+
+        auto parse_null() -> std::optional<Value> {
+            if (json_str.substr(pos, 4) == "null") {
+                pos += 4;
+                return Null{};
+            }
+            return {};
+        }
+        auto parse_true() -> std::optional<Value> {
+            if (json_str.substr(pos, 4) == "true") {
+                pos += 4;
+                return true;
+            }
+            return {};
+        }
+
+        auto parse_false() -> std::optional<Value> {
+            if (json_str.substr(pos, 5) == "false") {
+                pos += 5;
+                return false;
+            }
+            return {};
+        }
+
+        auto parse_string() -> std::optional<Value> {
+            pos++;
+            size_t end_pos = pos;
+            while (end_pos < json_str.size() && json_str[end_pos] != '"') {
+                end_pos++;
+            }
+            std::string str = std::string{ json_str.substr(pos, end_pos - pos) };
+            pos = end_pos + 1;
+            return str;
+        }
+
+        auto parse_array() -> std::optional<Value> {
+            pos++;
+            Array arr;
+            while (pos < json_str.size() && json_str[pos] != ']') {
+                auto value = parse_value();
+                arr.push_back(value.value());
+                parse_whitespace();
+                if (pos < json_str.size() && json_str[pos] == ',') {
+                    pos++;
+                }
+                parse_whitespace();
+            }
+            pos++;
+            return arr;
+        }
+
+        auto parse_object() -> std::optional<Value> {
+            pos++;
+            Object obj;
+            while (pos < json_str.size() && json_str[pos] != '}') {
+                auto key = parse_value();
+                parse_whitespace();
+                if (!std::holds_alternative<String>(key.value())) {
+                    return {};
+                }
+                parse_whitespace();
+                if (pos < json_str.size() && json_str[pos] == ':') {
+                    pos++;
+                }
+                parse_whitespace();
+                auto value = parse_value();
+                obj[std::get<String>(key.value())] = value.value();
+                parse_whitespace();
+                if (pos < json_str.size() && json_str[pos] == ',') {
+                    pos++;
+                }
+                parse_whitespace();
+            }
+            pos++;
+            return obj;
+        }
+
+        auto parse_number() -> std::optional<Value> {
+            size_t end_pos = pos;
+            while (end_pos < json_str.size() && (json_str[end_pos] == 'e' 
+            || json_str[end_pos] == '.' 
+            || std::isdigit(json_str[end_pos]))) {
+                end_pos++;
+            };
+            std::string num = std::string{json_str.substr(pos, end_pos - pos)};
+            auto is_Float = [](std::string str) ->bool {
+                return str.find('e') != str.npos || str.find(',') != str.npos;
+            };
+            pos = end_pos;
+            if (is_Float(num)) {
+                try {
+                    Float res = std::stod(num);
+                    return res;
+                }  catch(...) {
+                    return {};
+                }
+            } else {
+                try {
+                    Int res = std::stoi(num);
+                    return res;
+                } catch(...) {
+                    return {};
+                }
+            }
+        }
+
+        auto parse_value() -> std::optional<Value> {
+            parse_whitespace();
+            switch (json_str[pos]) {
+            case 'n':
+                return parse_null();
+            case 't':
+                return parse_true();
+            case 'f':
+                return parse_false();
+            case '"':
+                return parse_string();
+            case '[':
+                return parse_array();
+            case '{':
+                return parse_object();
+            default:
+                return parse_number();
+            }
+        }
+
+        auto parse() -> std::optional<Node> {
+            parse_whitespace();
+            auto value = parse_value();
+            if (!value) return {};
+            return Node { *value };
+        }
+
+    };
+
+    auto parser(std::string_view json_str) ->std::optional<Node> {
+        json_parser p{ json_str };
+        return p.parse();
+    }
+ 
+    class json_generator {
+      public:
+
+        static auto generate(const Node& node) -> std::string {
+            return std::visit(
+                [](auto&& arg) -> string {
+                    using T = decay_t<decltype(arg)>;
+                    if constexpr (std::is_same_v<T, Null>) {
+                        return "null";
+                    }
+                    else if constexpr (std::is_same_v<T, Bool>) {
+                        return arg ? "true" : "false";
+                    }
+                    else if constexpr (std::is_same_v<T, Int>) {
+                        return std::to_string(arg);
+                    }
+                    else if constexpr (std::is_same_v<T, Float>) {
+                        return std::to_string(arg);
+                    }
+                    else if constexpr (std::is_same_v<T, String>) {
+                        return generate_string(arg);
+                    }
+                    else if constexpr (std::is_same_v<T, Array>) {
+                        return generate_array(arg);
+                    }
+                    else if constexpr (std::is_same_v<T, Object>) {
+                        return generate_object(arg);
+                    }
+                }
+                ,node.value
+            );
+        }
+
+        static auto generate_string(const String& str) -> std::string {
+            std::string json_str = "";
+            json_str += '"';
+            json_str += str;
+            json_str += '"';
+            return json_str;
+        }
+
+        static auto generate_array(const Array& array) -> std::string {
+            std::string json_str = "";
+            json_str += '[';
+            for (const auto & node : array) {
+                json_str += generate(node);
+                json_str += ',';
+            }
+            if (!array.empty()) json_str.pop_back();
+            json_str += ']';
+            return json_str;
+        }
+
+        static auto generate_object(const Object& object) -> std::string {
+           std::string json_str = "";
+            json_str += '{';
+            for (const auto & [key, value] : object) {
+                json_str += generate_string(key);
+                json_str += ':';
+                json_str += generate(value);
+                json_str += ',';
+            }
+            if (!object.empty()) json_str.pop_back();
+            json_str += '}';
+            return json_str;
+        }
+    };
+
+    inline auto generate(const Node& node) -> std::string { return json_generator::generate(node); }
+
+
+    auto  operator << (std::ostream& out, const Node& t) ->std::ostream& {
+        out << json_generator::generate(t);
+        return out;
+    }
+}   
 
 
 #endif // _JSON_HPP_
--- main.cpp
+++ main.cpp
@@ -1,14 +1,14 @@
 #include "SkipList/skip_list.hpp"
 #include "Json/json.hpp"
-#include "ThreadPool/thread_pool.hpp"
+// #include "ThreadPool/thread_pool.hpp"
 
 using namespace std;
 
-mutex _m;
+// mutex _m; 
 
 int main(int argc, char** argv) {
     if (argc == 1) {
-        cout << __cplusplus << endl;
+        cout << "hello world!!" << endl;
         return 0;
     }
     
@@ -30,21 +30,37 @@ int main(int argc, char** argv) {
     }
 
     if (string(argv[1]) == "json") {
+        std::ifstream fin("myjson.json");
+        std::stringstream ss; ss << fin.rdbuf();
+        fin.close();
+        std::string s{ ss.str() };
+        auto x = json::parser(s).value();
+        // 
+        std::cout << x << "\n";
+        x["configurations"] = {json::Array {}};
+        x["configurations"].push({true});
+        x["files.associations"]["sbdx"] = {json::String {"ssss"}};
+        x["configurations"].push({json::Null {}});
+        x["version"] = { 114514LL };
+        std::cout << x << "\n";
+        std::ofstream fout("myjson.json");
+        fout << x;
+        fout.close();
         return 0;
     }
 
     if (string(argv[1]) == "threadpool") {
-        thread_pool pool(8);
-        int n = 20;
-        for (int i = 1; i <= n; i++) {
-            pool.submit([](int id) {
-                if (id % 2 == 1) {
-                    this_thread::sleep_for(0.2s);
-                }
-                unique_lock<mutex> lc(_m);
-                cout << "id : " << id << endl;
-                }, i);
-        }
+        // thread_pool pool(8);
+        // int n = 20;
+        // for (int i = 1; i <= n; i++) {
+        //     pool.submit([](int id) {
+        //         if (id % 2 == 1) {
+        //             this_thread::sleep_for(0.2s);
+        //         }
+        //         unique_lock<mutex> lc(_m);
+        //         cout << "id : " << id << endl;
+        //         }, i);
+        // }
         return 0;
     }
     cout << "please input skiplist, json or bufferpool" << endl;
